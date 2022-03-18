a924e2c4d5b9ec0aab3e4b092197e45f167126a0
hansoajani <101851255+hansoajani@users.noreply.github.com>
2022-03-18 11:10:24 -0400

    Add files via upload

---
+++ Services/Service.EventHandler/Commands/CommentCreateCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler.Commands
+{
+    public class CommentCreateCommand : INotification
+    {
+        public int UserId { get; set; }
+
+        public int PostId { get; set; }
+
+        public string? Annotation { get; set; }
+    }
+}
---
+++ Services/Service.EventHandler/CommentCreateEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Models.Domain;
+using Persistence.Database;
+using Service.EventHandler.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler
+{
+    public class CommentCreateEventHandler : INotificationHandler<CommentCreateCommand>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CommentCreateEventHandler(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Handle(CommentCreateCommand command, CancellationToken cancellationToken)
+        {
+            var users = _applicationDbContext.Users.Where(x => x.UserId == command.UserId).FirstOrDefault();
+
+            var posts = _applicationDbContext.Posts.Where(x => x.PostId == command.PostId).FirstOrDefault();
+
+            await _applicationDbContext.AddAsync(new Comment
+            {
+                User = users,
+                Post = posts,
+                Annotation = command.Annotation,
+            });
+
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
